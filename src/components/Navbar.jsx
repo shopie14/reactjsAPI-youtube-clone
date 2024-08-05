6c8a75e63c5c82f59761851ee5c7b10beff55078
shopie14 <sh.pinh14@gmail.com>
2024-08-05 11:50:54 +0700

    setting navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { Search } from "@mui/icons-material";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <Stack
+        direction="row"
+        alignItems="center"
+        p={2}
+        sx={{
+          position: "sticky",
+          background: "#000",
+          top: 0,
+          justifyContent: "space-between",
+        }}
+      >
+        <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+          <img src={logo} alt="logo" height={45} />
+        </Link>
+        <SearchBar />
+      </Stack>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
